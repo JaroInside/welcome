13a5b1600a5bf639c89cd068f126755176fb7873
jaroinside <jaro0116@gmail.com>
2018-10-23 04:06:02 +0900

    update - push 구현. 함수 분리.

--- update/index.js
+++ update/index.js
@@ -1,9 +1,23 @@
 'use strict';
 
+const util = require('../common/util');
+
+const SET = '$set';
+const PUSH = '$push';
+
+const BEHAVIOR_LIST = [
+  SET,
+  PUSH
+];
+
 function update(state, behavior) {
-  let nextState = Object.assign({}, state);
+  let nextState = util.shallowCopy(state);
+
+  if (util.checkProperty(behavior, SET)) return behavior[SET];
+  else if (util.checkProperty(behavior, PUSH)) behavior[PUSH].forEach(item => nextState.push(item));
+  
   for (let b in behavior) {
-    if (b !== '$set') nextState[b] = update(state[b], behavior[b]);
+    if (BEHAVIOR_LIST.findIndex(l => l === b) < 0) nextState[b] = update(state[b], behavior[b]);
   }
   return nextState;
 }
