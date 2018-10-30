3e8ff84d57df753461a86fc18bcf4cfa72e97566
jaroinside <jaro0116@gmail.com>
2018-10-30 15:24:57 +0900

    depth 체크시 set이면 값 return 해주는것 추가

--- update/index.js
+++ update/index.js
@@ -4,6 +4,7 @@ function update(state, behavior) {
   let nextState = Object.assign({}, state);
   for (let b in behavior) {
     if (b !== '$set') nextState[b] = update(state[b], behavior[b]);
+    else return behavior[b];
   }
   return nextState;
 }
